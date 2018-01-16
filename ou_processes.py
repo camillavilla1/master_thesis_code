df9f474c49af48bde4cf093298e51f048d0e5bfb
Camilla Stormoen <cst042@post.uit.no>
2018-01-16 14:34:38 +0100

    Can spawn multiple processes with random temperature

--- ou_processes.py
+++ ou_processes.py
@@ -3,6 +3,7 @@ import os
 import multiprocessing as mp
 import time
 import logging
+import random
 
 
 class ObservationUnit():
@@ -14,31 +15,52 @@ class ObservationUnit():
 		time.sleep(0.2)
 		print('%s * %s = %s' % (i, i, i*i))
 
+	""" See information on process """
 	def info(self):
 		#print("Module name: ", __name__)
 		print('Parent process:', os.getppid())
 		print("Process id: ", os.getpid())
 
+	""" Run process with traget to do_something """
 	def run(self, num_proc):
 		processes = []
-		mp.log_to_stderr()
-		logger = mp.get_logger()
-		logger.setLevel(logging.INFO)
+		#LOG WHETER PROCESS IS STARTING, RUNNING OR EXITING:
+		#mp.log_to_stderr()
+		#logger = mp.get_logger()
+		#logger.setLevel(logging.INFO)
 
 		for i in range(num_proc):
-			p = mp.Process(target=self.do_something, args=(i,))
+			#p = mp.Process(target=self.do_something, args=(i,))
+			p = mp.Process(target=self.temperature_sensor)
 			processes.append(p)
 
 		for x in processes:
 			x.start()
 
 
+	def camera_sensor(self):
+		pass
+
+	def weather_sensor(self):
+		pass
+
+	def temperature_sensor(self):
+		#pass
+		proc_name = mp.current_process().name
+		print("Doing something in %s." % proc_name)
+		rand_temp = random.randrange(1,20)
+		print(rand_temp)
+
+
 
 if __name__ == '__main__':
-	num_proc = 10
+	num_proc = 4
 	nProcess = ObservationUnit()
-	nProcess.info()
+	#nProcess.info()
+	#nProcess.temperature_sensor()
 	nProcess.run(num_proc)
 
 	#time.sleep(20)
 	#print("Exiting!")
+
+
