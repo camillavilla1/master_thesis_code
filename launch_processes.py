d266e73bf2da5705374ec35516bee05a77f4dc1d
Camilla Stormoen <cst042@post.uit.no>
2018-01-22 14:43:40 +0100

    Can use subprocess to simulate sensors

--- launch_processes.py
+++ launch_processes.py
@@ -1,7 +1,8 @@
 import subprocess
+import sys 
 
-
-for i in range(4):
+num_of_procs = sys.argv[1]
+for i in range(int(num_of_procs)):
 	process = subprocess.Popen(['python3', 'sim_one_ou_processes.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	output = process.communicate()
 	print(output[0])	
