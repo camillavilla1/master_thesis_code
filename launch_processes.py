c9d6b2c9fc3a0062dcec261d5c1094433cda22ab
Camilla Stormoen <cst042@post.uit.no>
2018-01-22 14:55:57 +0100

    Check if subprocess is terminated

--- launch_processes.py
+++ launch_processes.py
@@ -1,9 +1,29 @@
 import subprocess
 import sys 
+import os
 
-num_of_procs = sys.argv[1]
-for i in range(int(num_of_procs)):
-	process = subprocess.Popen(['python3', 'sim_one_ou_processes.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	output = process.communicate()
-	print(output[0])	
-#process = subprocess.run(['python3', 'sim_one_ou_processes.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def launch_processes():
+	num_of_procs = sys.argv[1]
+
+	for i in range(int(num_of_procs)):
+		process = subprocess.Popen(['python3', 'sim_one_ou_processes.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		output = process.communicate()
+		print(output[0])
+
+		proc_id = process.pid
+		print("Process ID is: ", process.pid)
+
+	process.kill()
+
+	poll = process.poll()
+	if poll == None:
+		print("Subprocess is alive!!")
+	else:
+		print("Subprocesses are terminated")
+
+
+if __name__ == '__main__':
+	launch_processes()
+
