fa859a18b029d7a643ada07062f324436cb91a4c
Camilla Stormoen <cst042@post.uit.no>
2018-01-23 10:59:36 +0100

    Implementing subprocesses etc

--- subprocess_test.py
+++ subprocess_test.py
@@ -1,24 +1,10 @@
 import subprocess
 import sys
-"""
-completed = subprocess.run(
-    ['ls', '-1'],
-    stdout=subprocess.PIPE,
-)
-print('returncode:', completed.returncode)
-print('Have {} bytes in stdout:\n{}'.format(
-    len(completed.stdout),
-    completed.stdout.decode('utf-8'))
-)
-"""
 
 
-if __name__ == '__main__':
-
-	num_of_proc = sys.argv[1]
+def run_subprocess(num_of_proc):
 	print("Run %d subprocess.." % int(num_of_proc))
-
-	for i in range(int(num_of_proc)):
+	for i in range(num_of_proc):
 		#process = subprocess.run(['python3', 'sim_one_ou_processes.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 		process = subprocess.Popen(['python3', 'sim_one_ou_processes.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -29,12 +15,19 @@ if __name__ == '__main__':
 		proc_id = process.pid
 		print("Process ID is: ", process.pid)
 
-
 	poll = process.poll()
 	if poll == None:
 		print("Subprocess is alive!!")
 	else:
 		print("Subprocesses are terminated")
+
+
+
+if __name__ == '__main__':
+
+	num_of_proc = sys.argv[1]
+	run_subprocess(int(num_of_proc))
+
 
 		
 
