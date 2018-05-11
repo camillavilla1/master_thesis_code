b6b21f21b0092cf7b0204e2c8bae12307b67bb2b
Camilla Stormoen <dc-milla@hotmail.com>
2018-05-11 14:55:38 +0200

    Experiments works 100 nodes 4 sends

--- master_thesis_code/cmd/server/server.go
+++ master_thesis_code/cmd/server/server.go
@@ -790,7 +790,7 @@ func (ou *ObservationUnit) getData() {
 				ou.SensorData.Data = tmp
 				ou.AccCount++
 				time.Sleep(time.Second * 60)
-				if ou.AccCount == 2 {
+				if ou.AccCount == 4 {
 					ou.ClusterHeadCount++
 					//go ChExperiments(os.Getpid(), ou.ClusterHeadCount, ou.ReceivedDataPkt)
 					//Accumulated data x times, elect a new leader..
